a7352d5c3a7d84f90b3428f9d4cb1cc80dad5ab2
naorwaiss <naorw@post.bgu.ac.il>
2024-06-21 19:58:49 +0300

    fix problem

--- src/drone_project/drone_project/get_data.py
+++ src/drone_project/drone_project/get_data.py
@@ -4,32 +4,20 @@ import csv
 import os
 from collections import deque
 
-#ros2 import
+# ROS2 import
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Float32MultiArray
-from std_msgs.msg import Float64
-
-
-from sensor_msgs.msg import MagneticField
-from rclpy.qos import QoSProfile, QoSReliabilityPolicy
-from sensor_msgs.msg import NavSatFix
-from mavros_msgs.msg import GPSRAW
-from sensor_msgs.msg import Imu
+from std_msgs.msg import Float32MultiArray, Float64
+from sensor_msgs.msg import MagneticField, NavSatFix, Imu
 from geometry_msgs.msg import TwistStamped
-from mavros_msgs.msg import Altitude  # Correct import for Altitude message
-
-
-
-
-
+from mavros_msgs.msg import GPSRAW, Altitude
 
 class Listener(Node):
     def __init__(self):
         super().__init__('get_data')
-        qos_profile = QoSProfile(depth=10, reliability=QoSReliabilityPolicy.RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT)
+        qos_profile = QoSProfile(depth=10, reliability=QoSReliabilityPolicy.BEST_EFFORT)
 
-##############################################statuse##########################################
+##############################################status##########################################
         self.topic_reception_status = {
             '/rpm_data': False,
             '/voltage_data': False,
@@ -37,18 +25,12 @@ class Listener(Node):
             '/optical_flow_data': False,
             '/converted_pose': False,
             '/mavros/gpsstatus/gps1/raw': False,
-            '/mavros/imu_data': False,
-            '/mavros/global_position/compass_hdg':False,
-            '/mavros/altitude':False
-
+            '/mavros/imu/data': False,
+            '/mavros/global_position/compass_hdg': False,
+            '/mavros/altitude': False
         }
 
-
-
-
-#######################################call for no gps functino #############################################
-
-
+#######################################call for no gps function #############################################
 
         # Subscription to '/rpm_data'
         self.subscription = self.create_subscription(
@@ -65,7 +47,6 @@ class Listener(Node):
             self.Voltege_motor_callback,
             qos_profile
         )
-
 
         # Subscription to '/temperature_data'
         self.subscription = self.create_subscription(
@@ -87,7 +68,7 @@ class Listener(Node):
         self.subscription = self.create_subscription(
             Imu,  # Change Float32MultiArray to Imu
             '/mavros/imu/data',  # Correct topic name
-            self.imu_data_coallback,  # Assuming you have a callback function named imu_data_coallback
+            self.imu_data_callback,  # Assuming you have a callback function named imu_data_callback
             qos_profile
         )
 
@@ -106,8 +87,8 @@ class Listener(Node):
             Float32MultiArray,
             '/converted_pose',
             self.pose_callback,
-            qos_profile)
-
+            qos_profile
+        )
 
         # Subscription to '/mavros/gpsstatus/gps1/raw' for GPS data
         self.subscription = self.create_subscription(
@@ -117,10 +98,9 @@ class Listener(Node):
             qos_profile
         )
 
-
-        # Subscription to '/mavros/global_position/compass_hdg' for GPS data
+        # Subscription to '/mavros/global_position/compass_hdg' for compass heading data
         self.subscription = self.create_subscription(
-            GPSRAW,
+            Float64,
             '/mavros/global_position/compass_hdg',
             self.compass_callback,
             qos_profile
@@ -133,7 +113,6 @@ class Listener(Node):
             self.velocity_body_callback,  # Callback function for velocity body data
             qos_profile
         )
-
 
 ########################################init###################################################3
 
@@ -161,25 +140,20 @@ class Listener(Node):
         self.m2 = None
         self.m3 = None
         self.m4 = None
-        self.temp_m1 =None
-        self.temp_m2 =None
-        self.temp_m3 =None
+        self.temp_m1 = None
+        self.temp_m2 = None
+        self.temp_m3 = None
         self.temp_m4 = None
         self.volt_m1 = None
         self.volt_m2 = None
-        self.volt_m3 =None
-        self.volt_m4 =None
+        self.volt_m3 = None
+        self.volt_m4 = None
         self.non_gps_altitude = None
-
 
 ###############################----function -------------------#############################
 
-
-
     def pose_callback(self, msg):
         if len(msg.data) == 6:
-            #self.time = float(msg.header.stamp.sec) + float(msg.header.stamp.nanosec) / 1e9
-
             x, y, z, roll, pitch, yaw = msg.data
             self.x_gps = x
             self.y_gps = y
@@ -187,114 +161,84 @@ class Listener(Node):
             self.roll = roll
             self.pitch = pitch
             self.yaw = yaw
-            #print(f"x: {self.x_gps}, y: {self.y_gps}, z: {self.z_gps}, roll: {self.roll}, pitch: {self.pitch}, yaw: {self.yaw}")
             self.topic_reception_status['/converted_pose'] = True
-
         else:
             self.get_logger().warn("Invalid data received on /converted_pose topic.")
 
-
     def gps_callback(self, msg):
         hdop = msg.eph
-        self.hdop = (float(hdop))/100
+        self.hdop = (float(hdop)) / 100
         self.topic_reception_status['/mavros/gpsstatus/gps1/raw'] = True
 
-
-
     def rpm_callback(self, msg):
-        # Assuming the message contains data for four motors
         if len(msg.data) == 4:
             self.m1, self.m2, self.m3, self.m4 = msg.data
-            # Now m1, m2, m3, and m4 contain the values from the rpm_data message
             self.topic_reception_status['/rpm_data'] = True
-
         else:
             self.get_logger().warn("Invalid data received on /rpm_data topic. Expected data for four motors.")
 
     def tmp_callback(self, msg):
-        # Assuming the message contains data for four motors
         if len(msg.data) == 4:
             self.temp_m1, self.temp_m2, self.temp_m3, self.temp_m4 = msg.data
             self.topic_reception_status['/temperature_data'] = True
         else:
-            self.get_logger().warn("Invalid data received on /temperature_data Expected data for four motors.")
-
-
-
+            self.get_logger().warn("Invalid data received on /temperature_data topic. Expected data for four motors.")
 
     def Voltege_motor_callback(self, msg):
-        # Assuming the message contains data for four motors
         if len(msg.data) == 4:
             self.volt_m1, self.volt_m2, self.volt_m3, self.volt_m4 = msg.data
             self.topic_reception_status['/voltage_data'] = True
         else:
-            self.get_logger().warn("Invalid data received on /temperature_data Expected data for four motors.")
+            self.get_logger().warn("Invalid data received on /voltage_data topic. Expected data for four motors.")
 
     def optic_flow_collback(self, msg):
-        # Assuming the message contains data for four motors
         if len(msg.data) == 2:
             self.x_camera, self.y_camera = msg.data
             self.topic_reception_status['/optical_flow_data'] = True
         else:
-            self.get_logger().warn("Invalid data received on //optical_flow_data.")
+            self.get_logger().warn("Invalid data received on /optical_flow_data topic.")
 
-    def imu_data_coallback(self, msg):
-        # Check if the angular_velocity field exists in the message
+    def imu_data_callback(self, msg):
         if msg.angular_velocity:
-            # Accessing angular velocity x, y, and z values
             self.angular_velocity_x = msg.angular_velocity.x
             self.angular_velocity_y = msg.angular_velocity.y
             self.angular_velocity_z = msg.angular_velocity.z
         else:
             self.get_logger().warn("Angular velocity data is missing in the IMU message.")
 
-        # Check if the linear_acceleration field exists in the message
         if msg.linear_acceleration:
-            # Accessing linear acceleration x, y, and z values
             self.linear_acceleration_x = msg.linear_acceleration.x
             self.linear_acceleration_y = msg.linear_acceleration.y
             self.linear_acceleration_z = msg.linear_acceleration.z
-
+            self.topic_reception_status['/mavros/imu/data'] = True
         else:
-            # Handle the case where linear_acceleration field is missing
             self.get_logger().warn("Linear acceleration data is missing in the IMU message.")
 
     def compass_callback(self, msg):
         self.compass = msg.data
         self.topic_reception_status['/mavros/global_position/compass_hdg'] = True
 
-
     def velocity_body_callback(self, msg):
-        # Assuming the message contains data for velocity in x, y, and z directions
-        if msg is not None:  # Check if message is not None
-            # Assuming you want to handle velocity data here
-            # Example: storing velocity values in some variables
-            x_velocity = msg.twist.linear.x
-            y_velocity = msg.twist.linear.y
-            z_velocity = msg.twist.linear.z
-            # Do something with the velocity data, such as storing it or performing calculations
-            # Example: Store velocity data in some variables or objects
-            self.x_velocity = x_velocity
-            self.y_velocity = y_velocity
-            self.z_velocity = z_velocity
-            # Update topic reception status
+        if msg is not None:
+            self.x_velocity = msg.twist.linear.x
+            self.y_velocity = msg.twist.linear.y
+            self.z_velocity = msg.twist.linear.z
             self.topic_reception_status['/mavros/local_position/velocity_body'] = True
         else:
             self.get_logger().warn("Invalid data received on /mavros/local_position/velocity_body.")
 
     def altitude_callback(self, msg):
         if msg is not None:
-            self.non_gps_altitude = msg.local  # Accessing the 'amsl' field
+            self.non_gps_altitude = msg.local
             self.topic_reception_status['/mavros/altitude'] = True
         else:
             self.get_logger().warn("Invalid data received on /mavros/altitude.")
-
 
 class Data_csv:
     def __init__(self, listener):
         self.listener = listener
         self._running = False
-        self.buffer_size = 50  # adjust the buffer size
+        self.buffer_size = 50
         self.buffer = []
 
     def start(self):
@@ -318,15 +262,15 @@ class Data_csv:
 
         with open(self.filename, mode='w', newline='') as file:
             csv_writer = csv.writer(file)
-            # Write the header - need to change the ordering
+            # Write the header
             csv_writer.writerow([
                 "m1_rpm", "m2_rpm", "m3_rpm", "m4_rpm",
                 "temp_m1", "temp_m2", "temp_m3", "temp_m4",
                 "volt_m1", "volt_m2", "volt_m3", "volt_m4",
                 "x_camera", "y_camera",
                 "Altitude",
-                "linear_acceleration_x", "linear_acceleration_y", "linear_acceleration_z",   #imu
-                "angular_velocity_x", "angular_velocity_y", "angular_velocity_z",  #imu
+                "linear_acceleration_x", "linear_acceleration_y", "linear_acceleration_z",  # imu
+                "angular_velocity_x", "angular_velocity_y", "angular_velocity_z",  # imu
                 "compass",
                 "roll", "pitch", "yaw",
                 "x_gps", "y_gps", "z_gps"
@@ -353,26 +297,20 @@ class Data_csv:
             time.sleep(1)
 
     def hdop_status(self):
-        #ok this function work - check the status of the hdop before start the flight
         while True:
-            if self.listener.hdop is not None and self.listener.hdop <= 1:
+            if self.listener.hdop is not None and self.listener.hdop <= 10:
                 break
             if self.listener.hdop is not None:
-                print(f"HDOP: {self.listener.hdop}. Waiting for HDOP to be less than or equal to 1.5.")
+                print(f"HDOP: {self.listener.hdop}. Waiting for HDOP to be less than or equal to 10.")
             else:
                 print("HDOP not received yet. Waiting...")
             time.sleep(1)
 
-        print("HDOP is less than or equal to 1.5. Starting data processing and printing.")
+        print("HDOP is less than or equal to 10. Starting data processing and printing.")
 
     def process_data(self):
-       # self.check_msg_arrive()  # check the msg that all arive- not work at the sitl..
-
-        self.hdop_status() #check th hdop status
-        #need to check here the status of the drone also
-        self.start_csv_file() #open the csv file
-
-
+        self.hdop_status()  # Check the HDOP status
+        self.start_csv_file()  # Open the CSV file
 
         while self._running:
             for i in range(self.buffer_size):
@@ -390,19 +328,14 @@ class Data_csv:
                     self.listener.roll, self.listener.pitch, self.listener.yaw,
                     self.listener.x_gps, self.listener.y_gps, self.listener.z_gps
                 ]
-                hz = 50
-                time.sleep(1/hz)   #time stop...
+                hz = 20
+                time.sleep(1 / hz)
                 self.buffer.append(data)
 
+                print(f"we are at iteration {i}")
 
-
-                print(f"we ar at iteration {i}")
-
-            self.flush_buffer_to_csv()  #after get bufer size data upload the data to the csv
+            self.flush_buffer_to_csv()  # After getting buffer size data, upload the data to the CSV
             print("Buffer flushed to CSV.")
-
-
-
 
 def main(args=None):
     rclpy.init(args=args)
@@ -418,7 +351,6 @@ def main(args=None):
         data_processor.stop()
         listener.destroy_node()
         rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
